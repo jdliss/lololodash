7704fea48f0dd80fad499303b48ac0db1151fbb6
Jonathan <jonacom@lissismore.com>
2016-06-20 21:31:02 -0600

    revise 4. min

--- min.js
+++ min.js
@@ -2,10 +2,13 @@ var _ = require("lodash");
 
 var min = function(collection) {
     var towns = { hot: [], warm: [] }
+
+    function isHot(temp) { return temp > 19 };
+
     _.forEach(collection, function(temps, name) {
-        if (_.every(temps, function(temp) { return temp > 19 })) {
+        if (_.every(temps, isHot)) {
             towns.hot.push(name);
-        } else if (_.some(temps, function(temp) { return temp > 19 })){
+        } else if (_.some(temps, isHot)) {
             towns.warm.push(name);
         }
     });
